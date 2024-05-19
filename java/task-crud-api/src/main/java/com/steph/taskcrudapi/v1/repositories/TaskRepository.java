e2a28ddd6a06f8a4633cd132d7d48c6c335b1b42
Stephen <macmanustephen@gmail.com>
2024-05-19 17:41:31 +0000

    Delete java/task-crud-api directory

--- java/task-crud-api/src/main/java/com/steph/taskcrudapi/v1/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.steph.taskcrudapi.v1.repositories;
-
-import com.steph.taskcrudapi.v1.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
